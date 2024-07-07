d7af413f2ad40ea7c71dadac9c9f2fa08b4bca8a
Vüsal Quluyev <vquluyev274@gmail.com>
2024-07-07 14:54:59 +0400

    first commit

---
+++ react_todo/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import TodoCreate from './components/TodoCreate';
+import TodoList from "./components/TodoList";
+import './App.css';
+
+
+function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (todoId) => {
+    setTodos([...todos.filter((todo) => todo.id !== todoId)]);
+    //console.log(extractedArray);
+  };
+
+  const updateTodo = (newTodo) => {
+
+    const updateTodos = todos.map((todo) => {
+      if (todo.id !== newTodo.id) {
+        return todo;
+      }
+      return newTodo;
+    })
+    setTodos([...updateTodos]);
+
+  };
+
+  console.log(todos);
+
+
+  return (
+
+    <div className="App">
+
+      <div className="main">
+        <TodoCreate onCreateTodo={createTodo} />
+        <TodoList todos={todos} onRemoveTodo={removeTodo} onUpdateTodo={updateTodo} />
+      </div>
+
+    </div>
+
+  );
+
+};
+
+export default App
